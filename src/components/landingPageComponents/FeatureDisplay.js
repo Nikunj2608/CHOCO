5356031d2bdd2aa702c9714206225acf32359cd4
Yash Dhiraj Oza <144452060+YashDhirajOza@users.noreply.github.com>
2024-09-08 21:58:03 +0530

    Add files via upload

---
+++ src/components/landingPageComponents/FeatureDisplay.js
@@ -0,0 +1,24 @@
+import { Sparkle, } from 'lucide-react';
+import Image from 'next/image';
+
+function FeatureDisplay() {
+    return (
+        <>
+            <section className="bg-slate-50 py-24 pb-16">
+                <div className='max-w-sm sm:max-w-2xl lg:max-w-3xl mx-auto flex flex-col gap-4'>
+                    <h2 className='tracking-tight font-bold text-center md:text-left text-3xl lg:text-5xl lg:leading-[3.5rem]'>
+                        All YOU NEED IS A  CHOCO 🤤
+                    </h2>
+                    <p className='font-semibold my-4 text-center md:text-left text-gray-700'>
+                    Ek aisi duniya mein kho jao, jahaan har chuski ek nayi khushi ka wada hai! 'Chuso Choco' ke saath pao chocolate ki velvety richness, jo zubaan pe melt ho jaaye aur chocolate dreams ki duniya mein le jaaye. Yeh sirf ice cream nahi, ek celebration hai, ek khushi jo aapke dil ko choo jaaye. Toh kyun settle karein ordinary ke liye, jab extraordinary ka mazaa le sakte hain?
+                    </p>
+
+                  
+                   
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default FeatureDisplay
